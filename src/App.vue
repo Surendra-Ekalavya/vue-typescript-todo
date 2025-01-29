ae8cb7fc5a366c50ff9c6a6e96b4b26bc6690ba8
Surendra-Ekalavya <surendrababu.gariganti@ekalavya.io>
2025-01-29 11:37:02 +0530

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+
+
+<template>
+<div>
+    <h1>My Todo</h1>
+    <h3>{{ completedAll }}</h3>
+</div>
+<!-- Show list -->
+ <!-- Prop data to child component -->
+<ItemList :Items="DataItemList" @changeItem="completedItem"></ItemList>
+</template>
+
+<script setup lang="ts">
+
+import type { ItemInterface } from "./Models/Items/Item.interface";
+//import item list Component 
+import ItemList from "./components/Items/ItemComponent.vue"
+
+import { computed, reactive} from "vue";
+//create data item
+const DataItemList: ItemInterface=reactive([
+{
+    id:1,
+    name:'coffee',
+    completed:true,
+},
+{
+    id:2,
+    name:'Books',
+    completed:false,
+},
+{
+    id:5,
+    name:'Pen',
+    completed:false,
+},
+{
+    id:4,
+    name:'Laptop',
+    completed:false,
+},
+]);
+const completedItem=(id)=>{
+   // alert('id+item'+  id);
+};
+const completedAll=computed(()=>{
+    const all=DataItemList.length;
+    let countCompleted=0;
+
+    DataItemList.forEach(element => {
+        if(element.completed){
+            countCompleted++;
+        }
+        
+    });
+    return countCompleted==all?'[all Todo is Completed]':'[No]';
+});
+
+</script>
---
+++ src/components/Items/ItemComponent.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <h2>Item List</h2>
+   </div>
+    <!-- Show items -->
+     <ul v-for="data in Items" :key="data.id">
+        <li>{{ data.name }} - <input type="checkbox" v-model="data.completed" @click="completedItems(data.id)"/></li>
+     </ul>
+     <!-- {{ Items }} -->
+</template>
+<script setup lang="ts">
+import type { ItemInterface } from '@/Models/Items/Item.interface';
+defineProps<{Items: ItemInterface[] }>();
+
+const emit=defineEmits<{(e:"changeItem",id:number):any}>(); 
+const completedItems=(id)=>{
+   emit('changeItem',id);
+}
+</script>
